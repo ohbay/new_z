5b8e413a3e8277a39a11b0523e0ccbdd83daeafc
ohbay <pauloghuetto@gmail.com>
2025-04-25 15:21:44 +0200

    test #1

--- script_commits.sh
+++ script_commits.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-for i in {1..1200}; do
+for i in {1..1095}; do
     echo "$i - $(date)" >> fichier.txt
     git add .
-    git commit -m "efficiency #$i"
+    git commit -m "test #$i"
     sleep 0.1
 done
